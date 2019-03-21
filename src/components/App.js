3196a59506b5b94963387fe36acc1101419256c5
Pablo <pablo.deyzaguirre@globalincubator.com>
2019-03-21 11:39:58 +0100

    Callback added to communicate child with parent

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import VideoList from './VideoList';
 
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -16,12 +16,16 @@ class App extends React.Component {
         
         this.setState({ videos: response.data.items});
     };
+        // object fecthed from the YouTube API
+    onVideoSelect= (video) => {
+        console.log('From the App!', video);
+    };
 
     render() {
         return (
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit} />
-            <VideoList videos={ this.state.videos} />
+            <VideoList onVideoSelect={this.onVideoSelect} videos={ this.state.videos} />
         </div>
         );
     }
